551770ed939dbcad9176c8fbc15ba1fbdc5dc595
Beto <betocastro1819@gmail.com>
2020-06-27 20:40:28 -0300

    Basic vertex and fragment shaders compilation

--- KnoxEngine/main.cpp
+++ KnoxEngine/main.cpp
@@ -3,7 +3,23 @@
 #include <stdio.h>
 
 
-void framebufferSizeCallback(GLFWwindow *window, int width, int height)
+const char* vertexShaderSource = 
+"#version 330 core\n"
+"layout (location = 0) in vec3 pos;\n"
+"void main()\n"
+"{\n"
+"	gl_Position = vec4(pos.x, pos.y, pos.z, 1.0);\n"
+"}\n";
+
+const char* fragmentShaderSource =
+"#version 330 core\n"
+"out vec4 FragColor;\n"
+"void main()\n"
+"{\n"
+"	FragColor = vec4(1.0f, 0.5f, 0.2f, 1.0);\n"
+"}\n";
+
+void framebufferSizeCallback(GLFWwindow* window, int width, int height)
 {
 	glViewport(0, 0, width, height);
 }
@@ -25,7 +41,7 @@ int main()
 	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3); 
 	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
 
-	GLFWwindow *window = glfwCreateWindow(800, 600, "Knox Engine", NULL, NULL);
+	GLFWwindow* window = glfwCreateWindow(800, 600, "Knox Engine", NULL, NULL);
 	if (!window)
 	{
 		printf("Failed to create GLFW windows\n");
@@ -43,6 +59,62 @@ int main()
 	glViewport(0, 0, 800, 600);
 	glfwSetFramebufferSizeCallback(window, framebufferSizeCallback);
 
+	float vertices[] = {
+		-0.5f, -0.5f, 0.0f,
+		 0.0f,  0.5f, 0.0f,
+		 0.5f, -0.5f, 0.0f
+	};
+
+	// Vertex Buffer Object
+	unsigned int vertexBufferObj;
+	glGenBuffers(1, &vertexBufferObj);
+	glBindBuffer(GL_ARRAY_BUFFER, vertexBufferObj);
+	glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+
+	// Vertex shader
+	unsigned int vertexShader;
+	vertexShader = glCreateShader(GL_VERTEX_SHADER);
+
+	glShaderSource(vertexShader, 1, &vertexShaderSource, NULL);
+	glCompileShader(vertexShader);
+
+	int success;
+	glGetShaderiv(vertexShader, GL_COMPILE_STATUS, &success);
+	if (!success)
+	{
+		char infoLog[512];
+		glGetShaderInfoLog(vertexShader, 512, NULL, infoLog);
+		printf("ERROR: Vertex shader compilation failed!\n");
+		printf("INFO_LOG: %s\n", infoLog);
+	}
+	else
+	{
+		printf("Vertex shader compiled successfully\n");
+	}
+
+
+	// Fragment Shader
+	unsigned int fragmentShader;
+	fragmentShader= glCreateShader(GL_FRAGMENT_SHADER);
+
+	glShaderSource(fragmentShader, 1, &fragmentShaderSource, NULL);
+	glCompileShader(fragmentShader);
+
+	glGetShaderiv(fragmentShader, GL_COMPILE_STATUS, &success);
+	if (!success)
+	{
+		char infoLog[512];
+		glGetShaderInfoLog(fragmentShader, 512, NULL, infoLog);
+		printf("ERROR: Fragment shader compilation failed!\n");
+		printf("INFO_LOG: %s\n", infoLog);
+	}
+	else
+	{
+		printf("Fragment shader compiled successfully\n");
+	}
+
+
+	// Render Loop
 	while (!glfwWindowShouldClose(window))
 	{
 		processInput(window);
