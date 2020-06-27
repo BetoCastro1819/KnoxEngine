d51cf5eb60eccb2ef7cf25e5d0a3b09312b6d80d
Beto <betocastro1819@gmail.com>
2020-06-27 17:58:20 -0300

    Hello GLFW window

--- KnoxEngine/main.cpp
+++ KnoxEngine/main.cpp
@@ -1,6 +1,60 @@
 #include <glad/glad.h>
+#include <GLFW/glfw3.h>
+#include <stdio.h>
+
+
+void framebufferSizeCallback(GLFWwindow *window, int width, int height)
+{
+	glViewport(0, 0, width, height);
+}
+
+void processInput(GLFWwindow* window)
+{
+	if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
+	{
+		glfwSetWindowShouldClose(window, true);
+	}
+}
 
 int main()
 {
+	glfwInit();
+	
+	// Setup to target the OpenGL version 3.3 core-profile
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3); 
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3); 
+	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
+	GLFWwindow *window = glfwCreateWindow(800, 600, "Knox Engine", NULL, NULL);
+	if (!window)
+	{
+		printf("Failed to create GLFW windows\n");
+		glfwTerminate();
+		return -1;
+	}
+	glfwMakeContextCurrent(window);
+
+	if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress))
+	{
+		printf("Failed to initialize GLAD\n");
+		return -1;
+	}
+
+	glViewport(0, 0, 800, 600);
+	glfwSetFramebufferSizeCallback(window, framebufferSizeCallback);
+
+	while (!glfwWindowShouldClose(window))
+	{
+		processInput(window);
+
+		// Render
+		glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
+		glClear(GL_COLOR_BUFFER_BIT);
+
+		glfwPollEvents();
+		glfwSwapBuffers(window);
+	}
+
+	glfwTerminate();
 	return 0;
 }
